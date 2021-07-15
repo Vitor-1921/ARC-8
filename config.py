696bf43f5d97a89ae1166785268c99108cde6309
Vitor-1921 <vaugusto1921@gmail.com>
2021-07-15 04:42:56 +0000

    att

--- config.py
+++ config.py
@@ -4,17 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-BACKEND = 'Discord'
+
+
+BACKEND = 'Discord'  
 BOT_IDENTITY = {
  'token': environ.get('DISCORD_TOKEN')
 }
-BOT_ADMINS = (environ.get('BOT_ADMIN'), )
-BOT_PREFIX = '!'
-BOT_PREFIX_OPTIONAL_ON_CHAT = True
+BOT_ADMINS = ('Vitor_1921#5397', ) 
 
 BOT_DATA_DIR = r'data'
 BOT_EXTRA_PLUGIN_DIR = r'plugins'
-BOT_EXTRA_BACKEND_DIR= r'backend'
+BOT_EXTRA_BACKEND_DIR= r'backend"
 
 BOT_LOG_FILE = r'errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
+
