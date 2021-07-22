213917d6ee44f4c88b16c7149556a19dec62dde4
Vitor-1921 <75869441+Vitor-1921@users.noreply.github.com>
2021-07-21 23:51:23 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
-
-BACKEND = 'Discord'  
+BACKEND = 'Discord'
 BOT_IDENTITY = {
  'token': environ.get('DISCORD_TOKEN')
 }
-BOT_ADMINS = ('Vitor_1921#5397', ) 
+BOT_ADMINS = (environ.get('BOT_ADMIN'), )
 BOT_PREFIX = '!'
-BOT_PREFIX_OPTIONAL_ON_CHAT = True  
-
+BOT_PREFIX_OPTIONAL_ON_CHAT = True
 
 BOT_DATA_DIR = r'data'
 BOT_EXTRA_PLUGIN_DIR = r'plugins'
@@ -21,4 +18,3 @@ BOT_EXTRA_BACKEND_DIR= r'backend'
 
 BOT_LOG_FILE = r'errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
-
